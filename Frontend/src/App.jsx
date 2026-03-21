b5941656dfd26d65f405b9823cf8de580a442c7c
dev-tanmay-jana <tanmayjanaeiilm@gmail.com>
2026-03-22 01:17:12 +0530

    final fix router + location

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css'
 import Navbar from './Components/Navbar';
-import { Route,Routes,BrowserRouter, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer";
 import About from "./Pages/About";
@@ -9,31 +9,35 @@ import Menu from "./Components/MenuSection";
 import AppDownload from "./Components/AppDownload";
 import ReserveTable from "./Pages/ReserveTable";
 import News from "./Components/News";
-import {AnimatePresence} from 'framer-motion'
+import { AnimatePresence } from 'framer-motion';
 import StoreContextProvider from "./Context/StoreContext";
 
-const App = () => {
+// 🔥 FIXED PART
+const AnimatedRoutes = () => {
+  const location = useLocation();
 
   return (
-    <>
-    <BrowserRouter>
-    <StoreContextProvider>
-    <Navbar/>
     <AnimatePresence mode="wait">
-     <Routes location={location} key={location.pathname}>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/menu' element={<Menu/>} />
-        <Route path='/blog' element={<News/>} />
-        <Route path='/app' element={<AppDownload/>} />
-        <Route path='/booking' element={<ReserveTable/>} />
-    </Routes>
+      <Routes location={location} key={location.pathname}>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/blog' element={<News />} />
+        <Route path='/app' element={<AppDownload />} />
+        <Route path='/booking' element={<ReserveTable />} />
+      </Routes>
     </AnimatePresence>
-    <Footer/>
-    </StoreContextProvider>
-    </BrowserRouter>
-    </>
-  )
-}
+  );
+};
 
-export default App
+const App = () => {
+  return (
+    <StoreContextProvider>
+      <Navbar />
+      <AnimatedRoutes /> {/* ✅ correct */}
+      <Footer />
+    </StoreContextProvider>
+  );
+};
+
+export default App;
