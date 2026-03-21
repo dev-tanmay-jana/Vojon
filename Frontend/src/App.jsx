c5d2a987a0a06925dba8cb39efe522f7dbcb467b
dev-tanmay-jana <tanmayjanaeiilm@gmail.com>
2026-03-22 01:14:18 +0530

    add Browser router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css'
 import Navbar from './Components/Navbar';
-import { Route, Routes, BrowserRouter, useLocation } from "react-router-dom";
+import { Route,Routes,BrowserRouter, useLocation } from "react-router-dom";
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer";
 import About from "./Pages/About";
@@ -9,37 +9,31 @@ import Menu from "./Components/MenuSection";
 import AppDownload from "./Components/AppDownload";
 import ReserveTable from "./Pages/ReserveTable";
 import News from "./Components/News";
-import { AnimatePresence } from 'framer-motion';
+import {AnimatePresence} from 'framer-motion'
 import StoreContextProvider from "./Context/StoreContext";
 
-// 🔥 NEW COMPONENT
-const AnimatedRoutes = () => {
-  const location = useLocation();
-
-  return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/menu' element={<Menu />} />
-        <Route path='/blog' element={<News />} />
-        <Route path='/app' element={<AppDownload />} />
-        <Route path='/booking' element={<ReserveTable />} />
-      </Routes>
-    </AnimatePresence>
-  );
-};
-
 const App = () => {
-  return (
-    <BrowserRouter>
-      <StoreContextProvider>
-        <Navbar />
-        <AnimatedRoutes /> {/* ✅ use here */}
-        <Footer />
-      </StoreContextProvider>
-    </BrowserRouter>
-  );
-};
 
-export default App;
+  return (
+    <>
+    <BrowserRouter>
+    <StoreContextProvider>
+    <Navbar/>
+    <AnimatePresence mode="wait">
+     <Routes location={location} key={location.pathname}>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/menu' element={<Menu/>} />
+        <Route path='/blog' element={<News/>} />
+        <Route path='/app' element={<AppDownload/>} />
+        <Route path='/booking' element={<ReserveTable/>} />
+    </Routes>
+    </AnimatePresence>
+    <Footer/>
+    </StoreContextProvider>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
