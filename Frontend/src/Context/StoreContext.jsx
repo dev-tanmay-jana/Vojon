b8dfb9055e14b183c4557f89973461ea3f6174cc
Tanmay Jana <tanmayjanaeiilm@gmail.com>
2026-03-22 10:47:13 +0530

    Update food items API endpoint

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -11,7 +11,7 @@ const StoreContextProvider = (props) => {
   const fetchFoodItems = async () => {
     try {
       const response = await axios.get(
-        "http://vojon.fwh.is/Vojon/Backend/Foodlist.php"
+        "http://vojon.42web.io/Backend/Foodlist.php"
       );
       setFoodList(response.data);
     } catch (error) {
