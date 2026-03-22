fb3c4c0897208ca17eb7b961552b7121dd0d04df
Tanmay Jana <tanmayjanaeiilm@gmail.com>
2026-03-22 11:12:20 +0530

    Update food items API endpoint to use HTTPS

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -11,7 +11,7 @@ const StoreContextProvider = (props) => {
   const fetchFoodItems = async () => {
     try {
       const response = await axios.get(
-        "/api/Foodlist.phpp"
+        "https://vojon.42web.io/Backend/Foodlist.php"
       );
       setFoodList(response.data);
     } catch (error) {
